3f0b16b4ea198a5607a709fe484bf7e54b2d26ce
Vartika Sharma <vrtikaaa1811@gmail.com>
2025-04-07 14:30:56 +0530

    setup the frontend/backend together

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+//import express from "express";
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World bitch");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
+//app.listen(5000);
