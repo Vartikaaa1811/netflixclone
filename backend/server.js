0398e13dd20db19b6262de644a79f2ace8b8ed4a
Vartika Sharma <vrtikaaa1811@gmail.com>
2025-04-08 23:51:01 +0530

    changes in frontend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World bitch");
 });
+app.get("/profile", (req, res) => {
+  res.send("Hello World bitch route 2");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Hello World bitch route 3");
+});
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
